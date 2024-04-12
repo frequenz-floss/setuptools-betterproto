afd965232dee03e71c471b0013117f5437bb65bc
Leandro Lucarella <luca-frequenz@llucax.com>
2024-04-12 11:13:28 -0300

    Add initial implementation of the plug-in
    This implementation is based on:
    https://github.com/frequenz-floss/frequenz-repo-config-python/blob/v0.x.x/src/frequenz/repo/config/setuptools/grpc_tools.py
    
    It only updates the code generation to use betterproto.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/setuptools_betterproto/__init__.py
+++ src/setuptools_betterproto/__init__.py
@@ -2,3 +2,11 @@
 # Copyright © 2024 Frequenz Energy-as-a-Service GmbH
 
 """A modern setuptools plugin to generate Python files from proto files using betterproto."""
+
+from ._command import CompileProto
+from ._config import ProtobufConfig
+
+__all__ = [
+    "CompileProto",
+    "ProtobufConfig",
+]
