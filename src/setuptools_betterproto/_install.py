c42bdac0c134cdbc4120472d485611d580100729
Leandro Lucarella <luca-frequenz@llucax.com>
2024-05-22 15:21:09 +0200

    Properly include generated Python files in the wheel
    When building the wheel, the setuptools file discovery feature runs
    before the `compile_betterproto` sub-command is run, so the files are
    not included in the resulting wheel.
    
    To overcome this issue we use a hack, by running the compile command
    early when the distribution options are being finalized, we make sure
    the files are present by the time the file discovery runs.
    
    This is very hacky though, and even when it seems to work well in all
    tested cases (calling setuptools` `setup()` directly, `build`,
    `pip install`, `pip install -e`), I wouldn't be very surprised if it
    breaks in the future, so we probably will have to have an eye on it.
    
    Unfortunately it seems there is no easy way to do this with setuptools:
    
    * https://github.com/pypa/setuptools/discussions/3180#discussioncomment-2422328
    * https://github.com/pypa/setuptools/issues/2591
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- src/setuptools_betterproto/_install.py
+++ src/setuptools_betterproto/_install.py
@@ -6,6 +6,13 @@
 The `finalize_distribution_options` function is called by setuptools to finalize the
 distribution options. It adds the `compile_betterproto` command to the build sub-commands
 and replaces the `sdist` command with a custom one that includes the proto files.
+
+It will also build the proto files early if the distribution is a binary distribution.
+This is necessary to include the generated Python files in the binary distribution,
+otherwise the file discovery runs before we manage to compile the proto files.
+
+We still need to hook the sub-command into the build command to make sure editable
+installs work correctly.
 """
 
 import logging
@@ -27,6 +34,7 @@ def finalize_distribution_options(dist: Distribution) -> None:
 
     1. Replace the sdist command with a custom one that includes the proto files.
     2. Add the `compile_betterproto` command to the build sub-commands.
+    3. If the distribution is a binary distribution, build the proto files early.
 
     Args:
         dist: The distribution object.
@@ -34,6 +42,20 @@ def finalize_distribution_options(dist: Distribution) -> None:
     config = _config.ProtobufConfig.from_pyproject_toml()
     replace_sdist_command(dist)
     add_build_subcommand_compile_betterproto(dist)
+
+    if not building_bdist(dist):
+        return
+
+    if not config.expanded_proto_files:
+        _logger.warning(
+            "No proto files found in %s with glob %s, skipping early automatic "
+            "compilation of proto files.",
+            config.proto_path,
+            config.proto_glob,
+        )
+        return
+
+    build_proto(dist)
 
 
 def add_build_subcommand_compile_betterproto(dist: Distribution) -> None:
@@ -46,3 +68,24 @@ def add_build_subcommand_compile_betterproto(dist: Distribution) -> None:
 def replace_sdist_command(dist: Distribution) -> None:
     """Replace the sdist command with a custom one that includes the proto files."""
     dist.cmdclass.update(sdist=_command.SdistWithProtoFiles)
+
+
+def building_bdist(dist: Distribution) -> bool:
+    """Check if the distribution is a binary distribution."""
+    if not isinstance(dist.script_args, Container):
+        return False
+    for arg in dist.script_args:
+        if arg.startswith("bdist"):
+            return True
+    return False
+
+
+def build_proto(dist: Distribution) -> None:
+    """Build the Python protobuf files."""
+    _logger.info(
+        "Compiling protobuf files early so they are included in the binary distribution."
+    )
+    compile_cmg = _command.CompileBetterproto(dist)
+    compile_cmg.initialize_options()
+    compile_cmg.finalize_options()
+    compile_cmg.run()
